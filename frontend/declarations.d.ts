02b0a536b3ab06af6a29f48386638603151bd27a
rihanas <rihanna.socials@gmail.com>
2024-11-04 05:50:24 +0300

    update config files

--- frontend/declarations.d.ts
+++ frontend/declarations.d.ts
@@ -3,3 +3,18 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
